f7e4aff445566f41a2eb8f85d0526641a75198f8
Eduardo Kanno <eduardok@versul.com.br>
2017-03-14 15:57:22 -0300

    copy jar Eduardo pc

---
+++ copy_jar_to_reports_eduardo.sh
@@ -0,0 +1,5 @@
+rm -rf /home/kanno/Documentos/Projects/reports2/node_modules/jasper-stream/bin/jasper-report.jar
+cp target/JasperReports-1.0-SNAPSHOT-jar-with-dependencies.jar /home/kanno/Documentos/Projects/reports2/node_modules/jasper-stream/bin/jasper-report.jar
+
+# rm -rf /home/andre/projects/node/jasper-stream/bin/jasper-report.jar
+# cp target/JasperReports-1.0-SNAPSHOT-jar-with-dependencies.jar /home/andre/projects/node/jasper-stream/bin/jasper-report.jar
